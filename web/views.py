2b141dbdb9502ccd070b4f867498ae81877c3fb4
Elliot Acuña <elliot.acuna@intelector.net>
2023-03-27 16:52:27 -0600

    Agregacion de templates para el ecommerce

--- web/views.py
+++ web/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from .models import Categoria, Productos
+
 # Create your views here.
+
+"""VISTAS PARA EL CATALOGO DE PRODUCTOS"""
+
+def index(request):
+    listaProductos = Productos.objects.all()
+    print(listaProductos)
+    context = {
+        'productos':listaProductos
+    }
+    return render(request,'index.html')
